9135225feab18868903623eaeb2a515ba70a7074
Gaurav Verma <114090891+briskgaurav@users.noreply.github.com>
2024-09-24 23:00:20 +0530

    fix bug

--- src/Components/Card/Cards.jsx
+++ src/Components/Card/Cards.jsx
@@ -3,7 +3,7 @@ import Card from './Card';
 
 function Cards({ task, handleDelete, handleEdit }) {
   return (
-    <div className=' flex items-center lg:overflow-hidden justify-center gap-4 lg:gap-8 p-4 flex-wrap'>
+    <div className='flex w-screen items-center lg:overflow-hidden justify-center gap-4 lg:gap-8 p-4 flex-wrap'>
       <div className='h-full pointer-events-none w-screen bg-transparent absolute left-0 flex items-center justify-center lg:justify-end lg:pr-52 text-zinc-800/50'>
         <h1 className='text-4xl lg:text-[8vw] mt-20 lg:m-0 font-bold'>Health-Care</h1>
       </div>
